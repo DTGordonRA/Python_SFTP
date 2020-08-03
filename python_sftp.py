0b6b40cacec1c9b58893db90731511a7cdad681c
DTGordonRA <David.Gordon@rookadvisors.com>
2020-08-03 14:21:57 -0600

    Added commented out username/password variant for sftp

--- python_sftp.py
+++ python_sftp.py
@@ -1,10 +1,19 @@
 import configparser
 import pysftp
 import sys
+import os
+import virtualenv
+
+venv_dir = os.path.join(os.path.dirname(__file__), 'sftp_env')
+virtualenv.create_environment(venv_dir)
+# TODO change bin to Scripts an activate_this.py to activate.bat in windows version
+exec(open(os.path.join(venv_dir, 'bin', 'activate_this.py')).read())
 
 config = configparser.ConfigParser()
 config.read('config.txt')
 
+# Use this line if you're using a username and password rather than an ssh key
+# with pysftp.Connection( host = config['host']["hostname"], username = config['user']['username'], password = config['user']['password']) as sftp:
 with pysftp.Connection( host = config['host']["hostname"], private_key = config['key']['location'], private_key_pass = config['key']['pass']) as sftp:
     # Go to directory specified in command line
     with sftp.cd('{}'.format(sys.argv[1])):
